53f10702f7b04a8a8b54f7ea389ce2d557d11a12
alifakbar111 <alifakbar111@gmail.com>
2023-12-12 22:34:57 +0700

    feat: setup project and create landing page

---
+++ src/components/MaxWidthWrapper.tsx
@@ -0,0 +1,18 @@
+import { cn } from "@/lib/utils";
+import { ReactNode } from "react";
+
+const MaxWidthWrapper = ({
+  className,
+  children,
+}: {
+  className?: string;
+  children: ReactNode;
+}) => {
+  return (
+    <div className={cn("max-w-7xl mx-auto px-4 sm:px-6 lg:px-8", className)}>
+      {children}
+    </div>
+  );
+};
+
+export default MaxWidthWrapper;
